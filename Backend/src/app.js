8ae51ef393b7adec976fa605f52cb98eebd3bf9b
nilaj575 <jananilaj718@gmail.com>
2026-07-05 12:47:21 +0530

    hi

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ const cookieParser=require("cookie-parser");
 const cors=require("cors");
 
 app.use(cors({
-    origin:"https://interview-ai-nilaj.vercel.app/login",
+    origin:"http://localhost:5173/",
     credentials:true
 }));
 app.use(express.json());
